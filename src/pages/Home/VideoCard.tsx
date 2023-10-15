af226288b8bcd5823df530dd7a93f259c3eba52d
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-15 01:48:39 -0400

    video card

---
+++ src/pages/Home/VideoCard.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import FeatureCard from "./FeatureCard.tsx";
+
+const VideoCard: FC<{
+  description: string;
+  bgClass: string;
+  title: string;
+  video: string;
+  className: string;
+}> = ({ description, bgClass, title, className }) => {
+  return (
+    <div className={className}>
+      <FeatureCard title={title} bgClass={bgClass} description={description}>
+        {/* <video
+          key={video}
+          className="w-[80%] h-full rounded-md opacity-80 shadow-lg md:w-[90%]"
+          autoPlay={true}
+          loop={true}
+          muted={true}
+          playsInline={true}
+          data-video="0"
+        >
+          <source key={video} src={video} type="video/mp4"></source>
+        </video> */}
+      </FeatureCard>
+    </div>
+  );
+};
+
+export default VideoCard;
